9b8a34608bc5dfc4dcb5c0f27a646734e91fd116
patel malvin <patelmalvin318@gmail.com>
2026-06-04 13:30:24 +0530

    fix chroma init and startup crash

--- retriever.py
+++ retriever.py
@@ -1,13 +1,8 @@
-from google import genai
 import chromadb
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-client_google = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
 
 client_chroma = chromadb.PersistentClient(
     path="chroma_database"
@@ -19,21 +14,22 @@ def get_collection():
     except:
         return client_chroma.create_collection("pdf_text_chunks")
 
+
 def search(query, session_id, k=3):
+
+    collection = get_collection()   # MUST be inside function logic
 
     response = client_google.models.embed_content(
         model="gemini-embedding-2",
         contents=query
     )
+
     query_embedding = response.embeddings[0].values
-    collection = get_collection()   
 
     results = collection.query(
         query_embeddings=[query_embedding],
         n_results=k,
-        where={
-            "session_id": str(session_id)
-        }
+        where={"session_id": str(session_id)}
     )
 
     return results
