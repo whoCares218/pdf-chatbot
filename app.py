12ee41ab1e9300b2335b316b6f574eba0be30d57
patel malvin <patelmalvin318@gmail.com>
2026-06-04 12:37:27 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -532,4 +532,6 @@ def upload_pdf():
     }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
